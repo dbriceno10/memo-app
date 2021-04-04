7518f1db84c80251e2c90ece736379e1a9aad91b
nombre <daniel.b100726@gmail.com>
2021-04-03 21:21:02 -0400

    video 7 obteniendo valores de un formulario

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
-window.onload = () => {//esto espera hasta que todo el DOM cargue y luego ejecuta el código
-    const parrafo = document.getElementById("text");
-    console.log(parrafo.innerHTML);
-    console.log(parrafo.innerText);
-    // parrafo.innerText = "Texto Actualizado"
-    parrafo.innerHTML = "<li>elemento1</li><li>elemento2</li>"
+// window.onload = () => {//esto espera hasta que todo el DOM cargue y luego ejecuta el código
+//     const parrafo = document.getElementById("text");
+//     console.log(parrafo.innerHTML);
+//     console.log(parrafo.innerText);
+//     // parrafo.innerText = "Texto Actualizado"
+//     parrafo.innerHTML = "<li>elemento1</li><li>elemento2</li>"
+// }
+// function clearForm() {
+//     document.getElementById("form-tareas").reset()
+//     //el método ".reset()" que estamos llamando se va a encargar de resetear el formulario
+// }
+
+
+// const capturar = () => {
+//     const dato = document.getElementById("tarea").value;
+//     console.log("dato " + dato)
+//     return dato
+// }
+
+const form = document.getElementById("form-tareas");
+form.onsubmit = (ev) => {
+    ev.preventDefault();
+    const tarea = document.getElementById("tarea").value;
+    //limpia lo que hay en el campo de la tarea
+    // todo.value = '';
+    document.getElementById("form-tareas").reset();
+    console.log(tarea);
+    
 }
